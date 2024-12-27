67494ff735498e9a17a4e602c598077898ad3c47
TitereZerep <victorperezveiga@gmail.com>
2024-12-27 08:29:44 -0300

    número e chute

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 alert('boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt ('escolha um número entre 1 e 10');
+let numeroSecreto = 5;
+let chute = 0;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-    alert(`Muito bem! voce descobriu o numero secreto (${numeroSecreto})`);
-} else {
-    alert('você errou :(')
-}
+while (chute != numeroSecreto) {
+    chute = prompt ('escolha um número entre 1 e 10');
+    
+    if (chute == numeroSecreto) {
+        alert(`Muito bem! voce descobriu o numero secreto (${numeroSecreto}) usando ${tentativas} tentativas`);
+    } else {
+        if (numeroSecreto > chute) {
+            alert(`o número secreto é maior que ${chute}`);
+         } else {
+            alert (`o número secreto é menor que ${chute}`);
+            }
+            tentativas++;
+        }
+    }
