32b7361f521c74248215c35c2c01b6236d0b326d
TitereZerep <victorperezveiga@gmail.com>
2024-12-29 16:50:55 -0300

    projeto jogoNumeroAleatorio

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-alert('boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = 0;
+alert('Boas-vindas ao jogo do número secreto!');
+let endNumber = 1000;
+let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+let chute;
 let tentativas = 1;
 
-while (chute != numeroSecreto) {
-    chute = prompt ('escolha um número entre 1 e 10');
-    
-    if (chute == numeroSecreto) {
-        alert(`Muito bem! voce descobriu o numero secreto (${numeroSecreto}) usando ${tentativas} tentativas`);
+while (chute !== numeroSecreto) {
+    chute = Number(prompt(`Escolha um número entre 1 e ${endNumber}`));
+
+    if (chute === numeroSecreto) {
+        break;
     } else {
         if (numeroSecreto > chute) {
-            alert(`o número secreto é maior que ${chute}`);
-         } else {
-            alert (`o número secreto é menor que ${chute}`);
-            }
-            tentativas++;
+            alert(`O número secreto é maior que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
         }
+        tentativas++;
     }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Muito bem! Você descobriu o número secreto (${numeroSecreto}) usando ${tentativas} ${palavraTentativa}.`);
